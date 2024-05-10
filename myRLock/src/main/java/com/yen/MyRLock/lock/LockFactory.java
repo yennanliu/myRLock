aad6e7bd9afff9d5f4ba566c2ecb9a09de75e119
yennanliu <f339339@gmail.com>
2024-05-10 08:26:30 +0800

    add lock, update model, enum

---
+++ myRLock/src/main/java/com/yen/MyRLock/lock/LockFactory.java
@@ -0,0 +1,29 @@
+package com.yen.MyRLock.lock;
+
+import com.yen.MyRLock.model.LockInfo;
+import org.redisson.api.RedissonClient;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class LockFactory {
+
+    Logger logger= LoggerFactory.getLogger(getClass());
+
+    //@Autowired
+    private RedissonClient redissonClient;
+
+    public Lock getLock(LockInfo lockInfo){
+        switch (lockInfo.getType()) {
+            case Reentrant:
+                return new ReentrantLock(redissonClient, lockInfo);
+            case Fair:
+                return new FairLock(redissonClient, lockInfo);
+            case Read:
+                return new ReadLock(redissonClient, lockInfo);
+            case Write:
+                return new WriteLock(redissonClient, lockInfo);
+            default:
+                return new ReentrantLock(redissonClient, lockInfo);
+        }
+    }
+}
--- myRLock/src/main/java/com/yen/MyRLock/model/ReleaseTimeoutStrategy.java
+++ myRLock/src/main/java/com/yen/MyRLock/model/ReleaseTimeoutStrategy.java
@@ -1,4 +1,38 @@
-//package com.yen.MyRLock.model;
+package com.yen.MyRLock.model;
+
+import com.yen.MyRLock.handler.release.ReleaseTimeoutHandler;
+
+// TODO : check if necessary to use enum
+public enum ReleaseTimeoutStrategy implements ReleaseTimeoutHandler {
+    ;
+
+    @Override
+    public void handle(LockInfo lockInfo) {
+
+        System.out.println("(ReleaseTimeoutStrategy) handle method ...");
+    }
+
+    // TODO : fix below
+//    /**
+//     * do nothing, proceed to following code logic
+//     */
+//    NO_OPERATION() {
+//        @Override
+//        public void handle(LockInfo lockInfo) {
+//            // do nothing
+//        }
+//    },
+//    /**
+//     * fail fast
+//     */
+//    FAIL_FAST() {
+//        @Override
+//        public void handle(LockInfo lockInfo) {
 //
-//public enum ReleaseTimeoutStrategy implements ReleaseTimeoutHandler {
-//}
+//            String errorMsg = String.format("Found Lock(%s) already been released while lock lease time is %d s", lockInfo.getName(), lockInfo.getLeaseTime());
+//            throw new KlockTimeoutException(errorMsg);
+//        }
+//    }
+
+
+}
