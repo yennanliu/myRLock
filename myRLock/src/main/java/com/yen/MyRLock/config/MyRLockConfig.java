445e160fce574734c0e5c01b114bbfaa9fec3296
yennanliu <f339339@gmail.com>
2024-05-11 16:07:50 +0800

    add config

---
+++ myRLock/src/main/java/com/yen/MyRLock/config/MyRLockConfig.java
@@ -0,0 +1,77 @@
+package com.yen.MyRLock.config;
+
+//@ConfigurationProperties(prefix = MyRLockConfig.PREFIX)
+public class MyRLockConfig {
+
+    public static final String PREFIX = "spring.MyRLock";
+    
+    //redisson
+    private String address;
+    private String password;
+    private int database=15;
+    private ClusterServer clusterServer;
+    private long waitTime = 60;
+    private long leaseTime = 60;
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public long getWaitTime() {
+        return waitTime;
+    }
+
+    public void setWaitTime(long waitTime) {
+        this.waitTime = waitTime;
+    }
+
+    public long getLeaseTime() {
+        return leaseTime;
+    }
+
+    public void setLeaseTime(long leaseTime) {
+        this.leaseTime = leaseTime;
+    }
+
+    public int getDatabase() {
+        return database;
+    }
+
+    public void setDatabase(int database) {
+        this.database = database;
+    }
+
+    public ClusterServer getClusterServer() {
+        return clusterServer;
+    }
+
+    public void setClusterServer(ClusterServer clusterServer) {
+        this.clusterServer = clusterServer;
+    }
+
+    public static class ClusterServer{
+
+        private String[] nodeAddresses;
+
+        public String[] getNodeAddresses() {
+            return nodeAddresses;
+        }
+
+        public void setNodeAddresses(String[] nodeAddresses) {
+            this.nodeAddresses = nodeAddresses;
+        }
+    }
+
+}
