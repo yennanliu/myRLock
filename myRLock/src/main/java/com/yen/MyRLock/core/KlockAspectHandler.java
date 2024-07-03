4dc1a93464366492cf020fc61debf8847020f484
yennanliu <f339339@gmail.com>
2024-07-03 19:08:53 +0800

    update KlockAspectHandler.java

--- myRLock/src/main/java/com/yen/MyRLock/core/KlockAspectHandler.java
+++ myRLock/src/main/java/com/yen/MyRLock/core/KlockAspectHandler.java
@@ -1,5 +1,9 @@
 // package com.yen.MyRLock.core;
 //
+// import com.yen.MyRLock.annotation.MyRLock;
+// import com.yen.MyRLock.annotation.MyRLockKey;
+//
+// import com.yen.MyRLock.handler.exception.MyRlockInvocationException;
 // import com.yen.MyRLock.lock.Lock;
 // import com.yen.MyRLock.lock.LockFactory;
 // import com.yen.MyRLock.model.LockInfo;
@@ -13,11 +17,7 @@
 // import org.slf4j.Logger;
 // import org.slf4j.LoggerFactory;
 // import org.springframework.beans.factory.annotation.Autowired;
-//// import org.springframework.boot.autoconfigure.klock.annotation.Klock;
-//// import org.springframework.boot.autoconfigure.klock.handler.KlockInvocationException;
-//// import org.springframework.boot.autoconfigure.klock.lock.Lock;
-//// import org.springframework.boot.autoconfigure.klock.lock.LockFactory;
-//// import org.springframework.boot.autoconfigure.klock.model.LockInfo;
+//
 // import org.springframework.core.annotation.Order;
 // import org.springframework.stereotype.Component;
 // import org.springframework.util.StringUtils;
@@ -28,228 +28,170 @@
 // import java.util.Objects;
 // import java.util.concurrent.ConcurrentHashMap;
 //
-/// **
-// * Content :给添加@KLock切面加锁处理
-// */
 // @Aspect
 // @Component
 // @Order(0)
 // public class KlockAspectHandler {
 //
-//    private static final Logger logger = LoggerFactory.getLogger(KlockAspectHandler.class);
+//     private static final Logger logger = LoggerFactory.getLogger(KlockAspectHandler.class);
 //
-//    @Autowired
-//    LockFactory lockFactory;
+//     @Autowired
+//     private LockFactory lockFactory;
 //
-//    @Autowired
-//    private LockInfoProvider lockInfoProvider;
+//     @Autowired
+//     private LockInfoProvider lockInfoProvider;
 //
-//    private final Map<String,LockRes> currentThreadLock = new ConcurrentHashMap<>();
+//     private final Map<String, LockRes> currentThreadLock = new ConcurrentHashMap<>();
 //
+//     @Around(value = "@annotation(klock)")
+//     public Object around(ProceedingJoinPoint joinPoint, MyRLock klock) throws Throwable {
+//         LockInfo lockInfo = lockInfoProvider.get(joinPoint, klock);
+//         String currentLock = getCurrentLockId(joinPoint, klock);
+//         currentThreadLock.put(currentLock, new LockRes(lockInfo, false));
+//         Lock lock = lockFactory.getLock(lockInfo);
+//         boolean lockResult = lock.acquire();
 //
-//    @Around(value = "@annotation(klock)")
-//    public Object around(ProceedingJoinPoint joinPoint, Klock klock) throws Throwable {
-//        LockInfo lockInfo = lockInfoProvider.get(joinPoint,klock);
-//        String curentLock = this.getCurrentLockId(joinPoint,klock);
-//        currentThreadLock.put(curentLock,new LockRes(lockInfo, false));
-//        Lock lock = lockFactory.getLock(lockInfo);
-//        boolean lockRes = lock.acquire();
+//         if (!lockResult) {
+//             handleLockTimeout(klock, joinPoint);
+//         }
 //
-//        //如果获取锁失败了，则进入失败的处理逻辑
-//        if(!lockRes) {
-//            if(logger.isWarnEnabled()) {
-//                logger.warn("Timeout while acquiring Lock({})", lockInfo.getName());
-//            }
-//            //如果自定义了获取锁失败的处理策略，则执行自定义的降级处理策略
-//            if(!StringUtils.isEmpty(klock.customLockTimeoutStrategy())) {
+//         currentThreadLock.get(currentLock).setLock(lock);
+//         currentThreadLock.get(currentLock).setRes(true);
 //
-//                return handleCustomLockTimeout(klock.customLockTimeoutStrategy(), joinPoint);
+//         return joinPoint.proceed();
+//     }
 //
-//            } else {
-//                //否则执行预定义的执行策略
-//                //注意：如果没有指定预定义的策略，默认的策略为静默啥不做处理
-//                klock.lockTimeoutStrategy().handle(lockInfo, lock, joinPoint);
-//            }
-//        }
+//     @AfterReturning(value = "@annotation(klock)")
+//     public void afterReturning(JoinPoint joinPoint, MyRLock klock) throws Throwable {
+//         String currentLock = getCurrentLockId(joinPoint, klock);
+//         releaseLock(klock, joinPoint, currentLock);
+//         cleanUpThreadLocal(currentLock);
+//     }
 //
-//        currentThreadLock.get(curentLock).setLock(lock);
-//        currentThreadLock.get(curentLock).setRes(true);
+//     @AfterThrowing(value = "@annotation(klock)", throwing = "ex")
+//     public void afterThrowing(JoinPoint joinPoint, MyRLock klock, Throwable ex) throws Throwable {
+//         String currentLock = getCurrentLockId(joinPoint, klock);
+//         releaseLock(klock, joinPoint, currentLock);
+//         cleanUpThreadLocal(currentLock);
+//         throw ex;
+//     }
 //
-//        return joinPoint.proceed();
-//    }
+//     private void handleLockTimeout(MyRLock klock, ProceedingJoinPoint joinPoint) throws Throwable {
+//         if (logger.isWarnEnabled()) {
+//             logger.warn("Timeout while acquiring Lock({})", klock.name());
+//         }
 //
-//    @AfterReturning(value = "@annotation(klock)")
-//    public void afterReturning(JoinPoint joinPoint, Klock klock) throws Throwable {
-//        String curentLock = this.getCurrentLockId(joinPoint,klock);
-//        releaseLock(klock, joinPoint,curentLock);
-//        cleanUpThreadLocal(curentLock);
-//    }
+//         if (!StringUtils.isEmpty(klock.customLockTimeoutStrategy())) {
+//             handleCustomTimeout(klock.customLockTimeoutStrategy(), joinPoint);
+//         } else {
+//             klock.lockTimeoutStrategy().handle(lockInfoProvider.get(joinPoint, klock), lockFactory.getLock(lockInfoProvider.get(joinPoint, klock)), joinPoint);
+//         }
+//     }
 //
-//    @AfterThrowing(value = "@annotation(klock)", throwing = "ex")
-//    public void afterThrowing (JoinPoint joinPoint, Klock klock, Throwable ex) throws Throwable {
-//        String curentLock = this.getCurrentLockId(joinPoint,klock);
-//        releaseLock(klock, joinPoint,curentLock);
-//        cleanUpThreadLocal(curentLock);
-//        throw ex;
-//    }
+//     private Object handleCustomTimeout(String timeoutHandler, ProceedingJoinPoint joinPoint) throws Throwable {
+//         Method currentMethod = ((MethodSignature) joinPoint.getSignature()).getMethod();
+//         Object target = joinPoint.getTarget();
+//         Method handleMethod;
+//         try {
+//             handleMethod = joinPoint.getTarget().getClass().getDeclaredMethod(timeoutHandler, currentMethod.getParameterTypes());
+//             handleMethod.setAccessible(true);
+//         } catch (NoSuchMethodException e) {
+//             throw new IllegalArgumentException("Illegal annotation param customLockTimeoutStrategy", e);
+//         }
 //
-//    /**
-//     * 处理自定义加锁超时
-//     */
-//    private Object handleCustomLockTimeout(String lockTimeoutHandler, JoinPoint joinPoint) throws
-// Throwable {
+//         try {
+//             return handleMethod.invoke(target, joinPoint.getArgs());
+//         } catch (IllegalAccessException e) {
+//             throw new MyRlockInvocationException("Fail to invoke custom lock timeout handler: " + timeoutHandler, e);
+//         } catch (InvocationTargetException e) {
+//             throw e.getTargetException();
+//         }
+//     }
 //
-//        // prepare invocation context
-//        Method currentMethod = ((MethodSignature)joinPoint.getSignature()).getMethod();
-//        Object target = joinPoint.getTarget();
-//        Method handleMethod = null;
-//        try {
-//            handleMethod = joinPoint.getTarget().getClass().getDeclaredMethod(lockTimeoutHandler,
-// currentMethod.getParameterTypes());
-//            handleMethod.setAccessible(true);
-//        } catch (NoSuchMethodException e) {
-//            throw new IllegalArgumentException("Illegal annotation param
-// customLockTimeoutStrategy",e);
-//        }
-//        Object[] args = joinPoint.getArgs();
+//     private void releaseLock(MyRLock klock, JoinPoint joinPoint, String currentLock) throws Throwable {
+//         LockRes lockRes = currentThreadLock.get(currentLock);
+//         if (Objects.isNull(lockRes)) {
+//             throw new NullPointerException("Please check whether the input parameter used as the lock key value has been modified in the method, which will cause the acquire and release locks to have different key values and throw null pointers. currentLockKey:" + currentLock);
+//         }
+//         if (lockRes.getRes()) {
+//             boolean releaseRes = currentThreadLock.get(currentLock).getLock().release();
+//             lockRes.setRes(false);
+//             if (!releaseRes) {
+//                 handleReleaseTimeout(klock, lockRes.getLockInfo(), joinPoint);
+//             }
+//         }
+//     }
 //
-//        // invoke
-//        Object res = null;
-//        try {
-//            res = handleMethod.invoke(target, args);
-//        } catch (IllegalAccessException e) {
-//            throw new KlockInvocationException("Fail to invoke custom lock timeout handler: " +
-// lockTimeoutHandler ,e);
-//        } catch (InvocationTargetException e) {
-//            throw e.getTargetException();
-//        }
+//     private void handleReleaseTimeout(MyRLock klock, LockInfo lockInfo, JoinPoint joinPoint) throws Throwable {
+//         if (logger.isWarnEnabled()) {
+//             logger.warn("Timeout while release Lock({})", lockInfo.getName());
+//         }
 //
-//        return res;
-//    }
+//         if (!StringUtils.isEmpty(klock.customReleaseTimeoutStrategy())) {
+//             handleCustomReleaseTimeout(klock.customReleaseTimeoutStrategy(), joinPoint);
+//         } else {
+//             klock.releaseTimeoutStrategy().handle(lockInfo);
+//         }
+//     }
 //
-//    /**
-//     *  释放锁
-//     */
-//    private void releaseLock(Klock klock, JoinPoint joinPoint,String curentLock) throws Throwable
-// {
-//        LockRes lockRes = currentThreadLock.get(curentLock);
-//        if(Objects.isNull(lockRes)){
-//            throw new NullPointerException("Please check whether the input parameter used as the
-// lock key value has been modified in the method, which will cause the acquire and release locks to
-// have different key values and throw null pointers.curentLockKey:" + curentLock);
-//        }
-//        if (lockRes.getRes()) {
-//            boolean releaseRes = currentThreadLock.get(curentLock).getLock().release();
-//            // avoid release lock twice when exception happens below
-//            lockRes.setRes(false);
-//            if (!releaseRes) {
-//                handleReleaseTimeout(klock, lockRes.getLockInfo(), joinPoint);
-//            }
-//        }
-//    }
+//     private void handleCustomReleaseTimeout(String releaseTimeoutHandler, JoinPoint joinPoint) throws Throwable {
+//         Method currentMethod = ((MethodSignature) joinPoint.getSignature()).getMethod();
+//         Object target = joinPoint.getTarget();
+//         Method handleMethod;
+//         try {
+//             handleMethod = joinPoint.getTarget().getClass().getDeclaredMethod(releaseTimeoutHandler, currentMethod.getParameterTypes());
+//             handleMethod.setAccessible(true);
+//         } catch (NoSuchMethodException e) {
+//             throw new IllegalArgumentException("Illegal annotation param customReleaseTimeoutStrategy", e);
+//         }
 //
-//    // avoid memory leak
-//    private void cleanUpThreadLocal(String curentLock) {
-//        currentThreadLock.remove(curentLock);
-//    }
+//         try {
+//             handleMethod.invoke(target, joinPoint.getArgs());
+//         } catch (IllegalAccessException e) {
+//      throw new MyRlockInvocationException("Fail to invoke custom release timeout handler: " + releaseTimeoutHandler, e);
+//         } catch (InvocationTargetException e) {
+//             throw e.getTargetException();
+//         }
+//     }
 //
-//    /**
-//     * 获取当前锁在map中的key
-//     * @param joinPoint
-//     * @param klock
-//     * @return
-//     */
-//    private String getCurrentLockId(JoinPoint joinPoint , Klock klock){
-//        LockInfo lockInfo = lockInfoProvider.get(joinPoint,klock);
-//        String curentLock= Thread.currentThread().getId() + lockInfo.getName();
-//        return curentLock;
-//    }
+//     private void cleanUpThreadLocal(String currentLock) {
+//         currentThreadLock.remove(currentLock);
+//     }
 //
-//    /**
-//     *  处理释放锁时已超时
-//     */
-//    private void handleReleaseTimeout(Klock klock, LockInfo lockInfo, JoinPoint joinPoint) throws
-// Throwable {
+//     private String getCurrentLockId(JoinPoint joinPoint, MyRLock klock) {
+//         LockInfo lockInfo = lockInfoProvider.get(joinPoint, klock);
+//         return Thread.currentThread().getId() + lockInfo.getName();
+//     }
 //
-//        if(logger.isWarnEnabled()) {
-//            logger.warn("Timeout while release Lock({})", lockInfo.getName());
-//        }
+//     private static class LockRes {
 //
-//        if(!StringUtils.isEmpty(klock.customReleaseTimeoutStrategy())) {
+//         private final LockInfo lockInfo;
+//         private MyRLock lock;
+//         private boolean res;
 //
-//            handleCustomReleaseTimeout(klock.customReleaseTimeoutStrategy(), joinPoint);
+//         LockRes(LockInfo lockInfo, boolean res) {
+//             this.lockInfo = lockInfo;
+//             this.res = res;
+//         }
 //
-//        } else {
-//            klock.releaseTimeoutStrategy().handle(lockInfo);
-//        }
+//         LockInfo getLockInfo() {
+//             return lockInfo;
+//         }
 //
-//    }
+//         MyRLock getLock() {
+//             return lock;
+//         }
 //
-//    /**
-//     * 处理自定义释放锁时已超时
-//     */
-//    private void handleCustomReleaseTimeout(String releaseTimeoutHandler, JoinPoint joinPoint)
-// throws Throwable {
+//         void setLock(MyRLock lock) {
+//             this.lock = lock;
+//         }
 //
-//        Method currentMethod = ((MethodSignature)joinPoint.getSignature()).getMethod();
-//        Object target = joinPoint.getTarget();
-//        Method handleMethod = null;
-//        try {
-//            handleMethod =
-// joinPoint.getTarget().getClass().getDeclaredMethod(releaseTimeoutHandler,
-// currentMethod.getParameterTypes());
-//            handleMethod.setAccessible(true);
-//        } catch (NoSuchMethodException e) {
-//            throw new IllegalArgumentException("Illegal annotation param
-// customReleaseTimeoutStrategy",e);
-//        }
-//        Object[] args = joinPoint.getArgs();
+//         boolean getRes() {
+//             return res;
+//         }
 //
-//        try {
-//            handleMethod.invoke(target, args);
-//        } catch (IllegalAccessException e) {
-//            throw new KlockInvocationException("Fail to invoke custom release timeout handler: " +
-// releaseTimeoutHandler, e);
-//        } catch (InvocationTargetException e) {
-//            throw e.getTargetException();
-//        }
-//    }
-//
-//    private class LockRes {
-//
-//        private LockInfo lockInfo;
-//        private Lock lock;
-//        private Boolean res;
-//
-//        LockRes(LockInfo lockInfo, Boolean res) {
-//            this.lockInfo = lockInfo;
-//            this.res = res;
-//        }
-//
-//        LockInfo getLockInfo() {
-//            return lockInfo;
-//        }
-//
-//        public Lock getLock() {
-//            return lock;
-//        }
-//
-//        public void setLock(Lock lock) {
-//            this.lock = lock;
-//        }
-//
-//        Boolean getRes() {
-//            return res;
-//        }
-//
-//        void setRes(Boolean res) {
-//            this.res = res;
-//        }
-//
-//        void setLockInfo(LockInfo lockInfo) {
-//            this.lockInfo = lockInfo;
-//        }
-//    }
-//
-//
+//         void setRes(boolean res) {
+//             this.res = res;
+//         }
+//     }
 // }
