4c616c502670b3d0d9e78a2438f53c3581f88244
yennanliu <f339339@gmail.com>
2024-07-06 15:13:27 +0800

    set default val for MyRLock annotation

--- myRLock/src/main/java/com/yen/MyRLock/annotation/MyRLock.java
+++ myRLock/src/main/java/com/yen/MyRLock/annotation/MyRLock.java
@@ -25,61 +25,61 @@ public @interface MyRLock {
    *
    * @return name
    */
-  String name();
+  String name() default "";
 
   /**
    * 锁类型，默认可重入锁
    *
    * @return lockType
    */
-  LockType lockType();
+  LockType lockType() default LockType.Reentrant;
 
   /**
    * 尝试加锁，最多等待时间
    *
    * @return waitTime
    */
-  long waitTime();
+  long waitTime() default Long.MIN_VALUE;
 
   /**
    * 上锁以后xxx秒自动解锁
    *
    * @return leaseTime
    */
-  long leaseTime();
+  long leaseTime() default Long.MIN_VALUE;
 
   /**
    * 自定义业务key
    *
    * @return keys
    */
-  String[] keys();
+  String[] keys() default {};
 
   /**
    * 加锁超时的处理策略
    *
    * @return lockTimeoutStrategy
    */
-  LockTimeoutStrategy lockTimeoutStrategy();
+  LockTimeoutStrategy lockTimeoutStrategy() default LockTimeoutStrategy.NO_OPERATION;
 
   /**
    * 自定义加锁超时的处理策略
    *
    * @return customLockTimeoutStrategy
    */
-  String customLockTimeoutStrategy();
+  String customLockTimeoutStrategy() default "";
 
   /**
    * 释放锁时已超时的处理策略
    *
    * @return releaseTimeoutStrategy
    */
-  ReleaseTimeoutStrategy releaseTimeoutStrategy();
+  ReleaseTimeoutStrategy releaseTimeoutStrategy() default ReleaseTimeoutStrategy.NO_OPERATION;
 
   /**
    * 自定义释放锁时已超时的处理策略
    *
    * @return customReleaseTimeoutStrategy
    */
-  String customReleaseTimeoutStrategy();
+  String customReleaseTimeoutStrategy() default "";
 }
